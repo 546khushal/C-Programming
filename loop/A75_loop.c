7555240640ff76e54b7ca4e5f403c26436acaa43
khushal <khushalsuthar546@gmail.com>
2024-11-15 00:50:35 +0530

    Updated C patterns and added new programs

--- loop/A75_loop.c
+++ loop/A75_loop.c
@@ -1,20 +1,29 @@
 //Auther:khushal kumar
 /*wap to print given pattern
 1       2       3
-        4       5
+        5       4
                 6
 */
 #include<stdio.h>
 
 void pattern(){
     int i,j,k,num=1;
-    for(i=3;i>=1;i--){
-        for(j=1;j<=3-i;j++){
+    for(i=1;i<=3;i++){
+        for(j=1;j<i;j++){
             printf("\t");
         } 
-        for(k=1;k<=i;k++){
+        for(k=i;k<=3;k++){
             printf("%d\t",num);
             num++;
+            if(num==4){
+                num++;
+            }
+            else if(num==6){
+                num-=2;
+            }
+            else if(num==5){
+                num++;
+            }
         }   
         printf("\n");
         
